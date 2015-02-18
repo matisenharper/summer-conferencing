c65cb9b0d771fb78fdf20d64869298cb7b7d802d
mharper5 <mharper5@cn01.cs.stedwards.edu>
2015-02-18 07:36:43 -0600

    php folder push

---
+++ php/login.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Cover Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../../TwitterBootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/login.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+	  <!--
+          <div class="masthead clearfix">
+            <div class="inner">
+	      -->
+              <h1 class="cover-heading">St Edward's Summer Conferencing</h1>
+              </br>
+	      <!--
+	      <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">Conferences</a></li>
+                  <li><a href="#">Residence Halls</a></li>
+                  <li><a href="#">Events</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+	  -->
+          <div class="inner cover">
+            <!--<h1 class="cover-heading">Summer Conferencing</h1>-->
+	    <h1 class="cover-heading"><img src="../../hilltoper.png"</h1>
+      
+
+
+      <form class="form-signin" action="user_home.php">
+        <h4 class="form-signin-heading">Please sign in</h4>
+        <label for="inputEmail" class="sr-only">Email Address</label>
+        <input type="text" id="inputEmail" class="form-control" placeholder="Email Address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+        </div>
+        <button class="btn btn-lg btn-default btn-block" type="submit">Sign in</button>
+      </form>
+
+</div>
+          <div class="mastfoot">
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
